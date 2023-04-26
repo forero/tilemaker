3aa635729b526cc36d0b3a6cdb626d2d8c50629c
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2023-04-26 09:18:17 -0700

    pal 5 script

--- bin/pal5_script.py
+++ bin/pal5_script.py
@@ -1,18 +1,31 @@
+# generic python
 import tempfile
 import shutil
 import os
 import numpy as np
+import argparse
+
+# from the astro comunity
+import healpy
+
+# from astropy
 from astropy.table import Table, vstack
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+
+# from desihub
 from desitarget.targets import encode_targetid
 from fiberassign.fba_tertiary_io import assert_tertiary_targ
+from fiberassign.fba_tertiary_io import assert_tertiary_prio
+from desisurvey.tileqa import lb2uv
+from desiutil import dust
 
 def create_tertiary_priority_dict():
     return {
-        "BRIGHT_PM_BOO3": {"PRIORITY_INIT": 8000, "NGOAL": 4},
-        "FAINT_NOPM_BOO3": {"PRIORITY_INIT": 7000, "NGOAL": 4},
-        "BRIGHT_PM_GC": {"PRIORITY_INIT": 6000, "NGOAL": 4},
-        "FAINT_NOPM_GC": {"PRIORITY_INIT": 5000, "NGOAL": 4},
-        "FILLER": {"PRIORITY_INIT": 4000, "NGOAL": 4},
+        "PAL5_GAIA" : {"PRIORITY_INIT" : 8000, "NGOAL" : 4},
+        "PAL5_NOGAIA" : {"PRIORITY_INIT" : 7000, "NGOAL" : 4},
+        "SGR_GAIA" : {"PRIORITY_INIT" : 6000, "NGOAL" : 4},
+        "SGR_NOGAIA" : {"PRIORITY_INIT" : 5000, "NGOAL" : 4},
     }
 
 def create_empty_data_dict():
@@ -47,7 +60,7 @@ def create_priorities_file(output_path, prognum):
     myd = create_empty_data_dict()
     populate_data_dict(mydict, myd)
     d = create_table_from_dict(myd)
-    d.write(outfn)
+    d.write(outfn, overwrite=True)
 
 def read_input_data(filename):
     d = Table.read(filename)
@@ -63,64 +76,143 @@ def read_input_data(filename):
     d["PMRA"][sel], d["PMDEC"][sel], d["REF_EPOCH"][sel] = 0., 0., 2015.5
     return d
 
-def prepare_data_table(d, prognum, np_rand_seed):
+def prepare_data_table(d, prognum, obsconds, goaltime, np_rand_seed):
     d["TARGETID"] = encode_targetid(release=8888, brickid=prognum, objid=np.arange(len(d)))
     d["SUBPRIORITY"] = np.random.uniform(size=len(d))
     d.meta["EXTNAME"] = "TARGETS"
     d.meta["FAPRGRM"] = "tertiary{}".format(prognum)
-    d.meta["OBSCONDS"] = "BRIGHT"
+    d.meta["OBSCONDS"] = obsconds
     d.meta["SBPROF"] = "PSF"
-    d.meta["GOALTIME"] = 600
+    d.meta["GOALTIME"] = goaltime
     d.meta["RANDSEED"] = np_rand_seed
     return d
 
-def create_targets_file(output_path, prognum, input_filename):
+def create_targets_file(output_path, prognum, obsconds, goaltime, input_filename):
     outfn = os.path.join(output_path, "tertiary-targets-{:04d}.fits".format(prognum))
     np_rand_seed = 1234
     np.random.seed(np_rand_seed)
     
     d = read_input_data(input_filename)
     d.meta["INPUTFN"] = input_filename
-    d = prepare_data_table(d, prognum, np_rand_seed)
-    d.write(outfn)
+    d = prepare_data_table(d, prognum, obsconds, goaltime, np_rand_seed)
+    d.write(outfn, overwrite=True)
 
-    assert_tertiary_targ(prognum, outfn)
 
-def main():
+# AR grid dither of ~1 patrol radius
+# AR fiber patrol diameter = 12mm
+# AR                       = 0.047 deg with taking 70.4 um/arcsec
+# AR we take 0.048 deg
+def get_centers(field_ra, field_dec, npt=5, rad=0.048):
+    vals = rad * np.arange(-10, 11)
+    if npt > vals.size ** 2:
+        raise ValueError("get_centers: npt={} is larger than {} -> need to re-write get_centers!".format(npt, vals.size ** 2))
+    ras, decs = [], []
+    for i in range(len(vals)):
+        ras += (field_ra + vals[i] / np.cos(np.radians(field_dec + vals))).tolist()
+        decs += (field_dec + vals).tolist()
+    ras, decs = np.array(ras), np.array(decs)
+    field_c = SkyCoord(field_ra * u.degree, field_dec * u.degree, frame="icrs")
+    cs = SkyCoord(ras * u.degree, decs * u.degree, frame="icrs")
+    ii = cs.separation(field_c).value.argsort()
+    ras, decs = ras[ii], decs[ii]
+    ras, decs = ras[:npt], decs[:npt]
+    ras[ras < 0] += 360
+    return ras, decs
+
+# AR adapted from https://github.com/desihub/desisurvey/blob/94b02bdae04137526bf98dcec0dca8bd29a231d3/py/desisurvey/tileqa.py#L525-L554 
+def get_ebv_meds(tiles):
+    nside = 512
+    theta, phi = healpy.pix2ang(nside, np.arange(12*nside**2))
+    la, ba = phi*180./np.pi, 90-theta*180./np.pi
+    from desiutil import dust
+    ebva = dust.ebv(la, ba, frame='galactic',
+                    mapdir=os.getenv('DUST_DIR')+'/maps', scaling=1)
+    if isinstance(tiles, Table):
+        ra = tiles['RA'].data
+        dec = tiles['DEC'].data
+    else:
+        ra = tiles['RA']
+        dec = tiles['DEC']
+    coord = SkyCoord(ra=ra*u.deg, dec=dec*u.deg,
+                     frame='icrs')
+    coordgal = coord.galactic
+    lt, bt = coordgal.l.value, coordgal.b.value
+    uvt = lb2uv(lt, bt)
+    fprad = 1.605
+    ebv_meds = np.zeros(len(tiles))
+    for i in range(len(tiles)):
+        ind = healpy.query_disc(nside, uvt[i], fprad*np.pi/180.)
+        ebv_meds[i] = np.median(ebva[ind])
+    return ebv_meds
+
+def create_tiles_table(tileids, ras, decs, program):
+    d = Table()
+    d["TILEID"] = tileids
+    d["RA"], d["DEC"] = ras, decs
+    d["PROGRAM"] = program.upper()
+    d["IN_DESI"] = True
+    d["EBV_MED"] = get_ebv_meds(d)
+    d["DESIGNHA"] = 0  # eddie s email from 9/6/2022 10:54am
+    return d
+
+def create_tiles_file(output_path, prognum, program, tileids):
+    ntile = len(tileids)
+    field_ra, field_dec = 229.5, 1.0
+
+    ras, decs = get_centers(field_ra, field_dec, npt=ntile)
+    ras, decs = ras.round(3), decs.round(3)
+
+    d = create_tiles_table(tileids, ras, decs, program)
+    
+    outfn = os.path.join(output_path, "tertiary-tiles-{:04d}.ecsv".format(prognum))
+    d.write(outfn, overwrite=True)
+
+def assert_files(output_path, prognum):
+    targetsfn = os.path.join(output_path, "tertiary-targets-{:04d}.fits".format(prognum))
+    prioritiesfn = os.path.join(output_path, "tertiary-priorities-{:04d}.ecsv".format(prognum))
+
+    assert_tertiary_prio(prognum, prioritiesfn, targetsfn)
+    assert_tertiary_targ(prognum, targetsfn)
+
+
+def main(out_dir="./"):
+    
+    
     # Step 1.
-    # Copy the input target file into a temporary directory
-
-    # Define the source file path (location A)
+    # Copy the input target file into the working directory directory
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    
     src_file_path = "/global/cfs/cdirs/desi/users/raichoor/fiberassign-desi2-pal5/v0/inputcats/Pal5andSgr_desi2sel.fits"
 
-    # Create a temporary directory
-    tmp_dir = tempfile.mkdtemp()
+    # Construct the destination file path inside the output directory
+    dst_file_path = os.path.join(out_dir, os.path.basename(src_file_path))
 
-    # Construct the destination file path inside the temporary directory
-    dst_file_path = os.path.join(tmp_dir, os.path.basename(src_file_path))
-
-    # Copy the file from location A to the temporary directory
+    # Copy the file from location A to the output directory
     shutil.copy(src_file_path, dst_file_path)
 
-    print(f"Temporary directory: {tmp_dir}")
+    print(f"Output directory: {out_dir}")
     print(f"File copied to: {dst_file_path}")
     
-    
     # Step 2. Create the priorities.
-    create_priorities_file(tmp_dir, 9998)
-    create_priorities_file(tmp_dir, 9999)
+    create_priorities_file(out_dir, 9998)
+    create_priorities_file(out_dir, 9999)
 
     # Step 3. Create the targets
-    create_targets_file(tmp_dir, 9998, dst_file_path)
-    create_targets_file(tmp_dir, 9999, dst_file_path)
+    create_targets_file(out_dir, 9998, "BRIGHT", 600, dst_file_path)
+    create_targets_file(out_dir, 9999, "DARK", 1800, dst_file_path)
 
-    
-    
+    # Step 4. Create the tile files
+    create_tiles_file(out_dir, 9998, "BRIGHT", [999990, 999991, 999992, 999993, 999994])
+    create_tiles_file(out_dir, 9999, "DARK", [999995, 999996, 999997, 999998, 999999])
+
+    # Step 5. Assert priorities and target files
+    assert_files(out_dir, 9998)
+    assert_files(out_dir, 9998)
+
 if __name__ == "__main__":
-    main()
-
-
-
-
-# Deleting the temporary file
-#shutil.rmtree(tmp_dir)
+    parser = argparse.ArgumentParser(description='Create priorities, tiles and targets for fiberassign from an input target file.')
+    parser.add_argument('out_dir', type=str, help='Output path to write the priorities, tiles and targets')
+    
+    args = parser.parse_args()
+    main(out_dir=args.out_dir)
